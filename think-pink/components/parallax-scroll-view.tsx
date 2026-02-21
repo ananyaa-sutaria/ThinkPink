ce26cba0e60a04f506b30b9eb215a06e55914594
thegalacticlor <64993902+thegalacticlor@users.noreply.github.com>
2026-02-21 16:00:27 -0500

    The big push

--- think-pink/components/parallax-scroll-view.tsx
+++ think-pink/components/parallax-scroll-view.tsx
@@ -68,12 +68,10 @@ const styles = StyleSheet.create({
   },
   header: {
     height: HEADER_HEIGHT,
-    overflow: 'hidden',
   },
   content: {
     flex: 1,
     padding: 32,
     gap: 16,
-    overflow: 'hidden',
   },
 });
